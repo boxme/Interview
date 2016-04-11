269d177bbfed42327128fa0c549d6d6f0053af9e
boxme <desmond.ng.yang.yi@gmail.com>
2016-04-11 22:44:34 +0800

    Commit book solution for q3

--- cracking/soln/search_sort/q3.java
+++ cracking/soln/search_sort/q3.java
@@ -51,3 +51,66 @@ public static int binarySearchCircularArray(int[] array, int lo, int hi, int num
 	return binarySearchCircularArray(array, mid + 1, hi, num);
     }
 }
+
+// Correct answer
+public static int binarySearch(int[] array, int lo, int hi, int num)
+{
+    if (array == null) -1;
+
+    if (lo > hi) return -1;
+
+    int mid = (hi - lo) / 2 + lo;
+
+    if (array[mid] == num) return mid;
+    
+    // Left side is properly ordered
+    if (array[lo] < array[mid])
+    {
+	if (array[lo] <= num && num <= array[mid])
+	{
+	    // Search left
+	    return binarySearch(array, lo, mid - 1, num);
+	}
+	else
+	{
+	    // Search right
+	    return binarySearch(array, mid + 1, hi, num);
+	}
+    }
+    // Right side is properly ordered
+    else if (array[mid] < array[right])
+    {
+	if (array[mid] <= num && num <= array[hi])
+	{
+	    // Search right
+	    return binarySearch(array, mid + 1, hi, num);
+	}
+	else
+	{
+	    // Search left
+	    return binarySearch(array, lo, mid - 1, num);
+	}
+    }
+    // Left side is all repeat
+    else if (array[lo] == array[mid])
+    {
+	if (array[mid] != array[hi)
+	{
+	    // Search right
+	    return binarySearch(array, mid + 1, hi, num);
+	}
+	else
+	{
+	    // Takes care of an array like [7, 7, 7, 4, 5, 6, 7, 7]
+	    // Search for both halves
+	    int result = binarySearch(array, lo, mid - 1, num);
+	    if (result == -1)
+	    {
+		return binarySearch(array, mid + 1, hi, num);
+	    }
+	    return result;
+	}
+    }
+
+    return -1;
+}
