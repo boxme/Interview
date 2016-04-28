80a15400a82613bad68db936ef255da27f3a03ce
boxme <desmond.ng.yang.yi@gmail.com>
2016-04-28 10:45:31 +0800

    Memory efficient solution

--- elements/soln/chapter_11_Search/qn_11_4.java
+++ elements/soln/chapter_11_Search/qn_11_4.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
 class qn_11_4 {
+    
+    // Hashmap method that requires O(n) extra space, gives O(n) complexity
     public Pair searchPairInAbsSortedArray(int[] array, int k) {
         Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
 
@@ -23,5 +25,31 @@ class qn_11_4 {
             this.x = x;
             this.y = y;
         }
+    }
+
+    // O(n) complexity & O(1) extra space method. But will only work on normal sorted array
+    public static Pair searchPairInNormalSortedArray(int[] array, int k)
+    {
+	int lo = 0;
+	int hi = array.length - 1;
+
+	while (lo < hi)
+	{
+	    int sum = array[lo] + array[hi];
+	    if (sum == k)
+	    {
+		return new Pair(lo, hi);
+	    }
+	    else if (sum > k)
+	    {
+		hi--;
+	    }
+	    else
+	    {
+		lo--;
+	    }
+	}
+
+	return new Pair(-1, -1);
     }
 }
