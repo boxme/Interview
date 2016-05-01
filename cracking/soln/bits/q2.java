0dde393b331c5d1ae704d2de6bc35c3fcc0a8c90
boxme <desmond.ng.yang.yi@gmail.com>
2016-05-02 01:10:24 +0800

    Commit book solutions

--- cracking/soln/bits/q2.java
+++ cracking/soln/bits/q2.java
@@ -2,5 +2,33 @@ import java.util.*;
 
 public class q2
 {
-    
+    public static String printBinaryFromDouble(double num)
+    {
+	if (num >= 1 || num <= 0) return "ERROR";
+
+	StringBuilder binary = new StringBuilder();
+	binary.append(".");
+
+	while (num > 0)
+	{
+	    // Set a limit on length: cannot be more than 32 bits
+	    if (binary.length() >= 32)
+	    {
+		return "ERROR";
+	    }
+
+	    num = 2 * num;
+	    if (num >= 1)
+	    {
+		binary.append(1);;
+		num -= 1;
+	    }
+	    else
+	    {
+		binary.append(0);
+	    }
+	}
+
+	return binary.toString();
+    }
 }
--- cracking/soln/bits/q3.java
+++ cracking/soln/bits/q3.java
@@ -2,31 +2,88 @@ import java.util.*;
 
 public class q3
 {
-    public int getNextSmallestNumWithSameNumOfBits(int original)
+    public int getNextSmallestNumWithSameNumOfBits(int n)
     {
-	// Find the LSB
-	int lsb = original & (-original);
-	
-	// Turn off lsb
-	original = original & (~lsb);
+	int c = n;
+	int c0 = 0;
+	int c1 = 0;
 
-	// shift lsb to the right by 1
-	lsb = lsb >> 1;
-	
-	// Shifting the lsb of original to the right will give the next smallest
-	original = original | lsb;
+	// Find trailing ones
+	while ((c & 1 == 1))
+	{
+	    c1++;
+	    c >>= 1;
+	}
 
-	return original;
+	// Find zeros left of the trailing ones block
+	while ((c & 1) == 0 && (c != 0)
+	{
+	    c0++;
+	    c >>= 1;
+	}
+
+
+	int p = c1 + c0;
+
+	// Flip rightmost non-trailing 1
+	int mask = ~(1 << p);
+	n &= mask;
+
+	// Clear all 1s to the right of p
+	mask = ~0;
+	mask <<= p;
+	n &= mask;
+
+	// Insert ones immediately to the right of p
+	mask = (1 << (c1 + 1)) - 1;
+	mask <<= c0;
+
+	n |= mask;
+
+	return n;
     }
 
-    public int getNextBiggestNumWithSameNumOfBits(int original)
+    public int getNextBiggestNumWithSameNumOfBits(int n)
     {
-	// Find the LSB
-	int lsb = original ^ (-original);
+	int c = n;
+	int c0 = 0;
+	int c1 = 0;
+	
+	// Find zeros to right of p
+	while (((c & 1) == 0) && (c != 0))
+	{
+	    c0++;
+	    c >>= 1; 
+	}
 
-	// Turn off LSB
-	original = original ^ (~lsb);
+	// Find 1s to the right of p
+	while ((c & 1) == 1)
+	{
+	    c1++;
+	    c >>= 1;
+	}
 
+	// Error if 111111 000000 as there is no bigger number
+	if (c0 + c1 == 31 || c0 + c1 == 0)
+	{
+	    return -1;
+	}
 
+	int p = c0 + c1;
+
+	// Flip rightmost non-trailing zero
+	int mask = 1 << p;
+	n |= mask;
+
+	// Clear all bits to the right of p
+	mask = ~0;
+	mask <<= p;
+	n &= mask;
+
+	// Insert ones to the right
+	mask = (1 << c1) - 1;
+	n |= mask;
+
+	return n;
     }
 }
