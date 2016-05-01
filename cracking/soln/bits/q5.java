44ab735eae0066e3ab27e13cb740fdf2e99143c5
boxme <desmond.ng.yang.yi@gmail.com>
2016-05-02 01:59:42 +0800

    Commit more bits manipulation solutions from the book

--- cracking/soln/bits/q5.java
+++ cracking/soln/bits/q5.java
@@ -22,4 +22,22 @@ public class q5
 
 	return count;
     }
+
+    // Book solution
+    public static int bitSwapRequired(int a, int b)
+    {
+	// XOR to get all the bits differences
+	int diff = a ^ b;
+
+	// Count number of bits in diff
+	// i = i &  (i - 1) is to clear the LSB
+
+	int count = 0;
+	for (int i = diff; i != 0; i &= (i - 1))
+	{
+	    count++;
+	}
+
+	return count;
+    }
 }
--- cracking/soln/bits/q6.java
+++ cracking/soln/bits/q6.java
@@ -2,8 +2,13 @@ import java.util.*;
 
 public class q6
 {
-    public static void swapBits(int n)
+    public static int swapBits(int n)
     {
-	n = (n >> 1) | (n << 1);
+	int onesInOddPos = 0xAA;
+	int onesInEvenPos = 0x55;
+	
+	n = ((n & onesInOddPos) >> 1) | ((n & onesInEvenPos) << 1);
+
+	return n;
     }
 }
