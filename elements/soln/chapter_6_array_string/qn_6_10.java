0a2256f053a12622ab750e6f6e0a984d3e0c8e42
boxme <desmond.ng.yang.yi@gmail.com>
2016-06-30 00:39:27 +0800

    Add solution

--- elements/soln/chapter_6_array_string/qn_6_10.java
+++ elements/soln/chapter_6_array_string/qn_6_10.java
@@ -1,7 +1,10 @@
 import java.util.*;
 
+// Assumption: Must restore perm array back
+
 public class qn_6_10 {
     
+    // O(n2) complexity but constant space complexity
     public static int[] permuteArray(int[] A, int[] perm) {
         int len = A.length;
 
@@ -31,6 +34,29 @@ public class qn_6_10 {
 	    }
 	}
 
+	return A;
+    }
+
+    // O(n) complexity, but inplicit O(n) space complexity to restore the perm array
+    public static int[] permuateArray(int[] A, int[] perm) {
+        int len = A.length;
+	int[] copyPerm = Arrays.copyOf(perm, len);
+
+	for (int i = 0; i < len; ++i) {
+	    int currPos = i;
+	    int curr = A[currPos];
+
+	    while (perm[currPos] != -1) {
+	        int nextPos = perm[currPos];
+		perm[currPos] = -1;
+		int temp = A[nextPos];
+		A[nextPos] = curr;
+		curr = temp;
+	        currPos = nextPos;
+	    }
+	}
+
+	perm = copyPerm;
 	return A;
     }
 }
