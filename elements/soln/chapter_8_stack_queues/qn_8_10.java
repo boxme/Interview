45c6eb2fe28131b554d049c37b5db619e1dded2e
desmond <desmond@replaid.com>
2016-07-20 01:26:35 +0800

    Fix bug in circuler queue

--- elements/soln/chapter_8_stack_queues/qn_8_10.java
+++ elements/soln/chapter_8_stack_queues/qn_8_10.java
@@ -31,8 +31,6 @@ private class CircularQueue {
         if (size == 0) throw new IllegalException("Queue is empty");
 
         head = (head + 1) % size;
-        if (head > tail) throw new IllegalException("Size underflow");
-
         int value = array[head];
         size--;
         return value;
